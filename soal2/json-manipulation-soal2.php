b3b2a930f61b89da7f23c1485dc019adfae6901b
Whydandrian <whydandrian@gmail.com>
2021-01-15 00:38:50 +0700

    soal2 nomor 1 selesai

--- soal2/json-manipulation-soal2.php
+++ soal2/json-manipulation-soal2.php
@@ -34,12 +34,35 @@ $encoded = json_decode($dataJson, true);
 // }
 echo "1. Find users who doesn't have any phone numbers.<br><br>";
 for ($a = 0; $a < count($encoded); $a++) {
-  foreach ($encoded[$a]['profile']['phones'] as $key => $value) {
-    echo "$key : $value<br>";
+
+  if (count($encoded[$a]['profile']['phones']) < 1) {
+    echo 'Data USER ID : ' . $a . '<br>';
+    echo '&nbsp;============<br>';
+
+    foreach ($encoded[$a] as $key => $value) {
+      echo "&nbsp;&nbsp;&nbsp;&nbsp;$key :<br>";
+
+      if (is_array($value) or is_object($value)) {
+        foreach ($value as $data => $valuedata) {
+          echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $data . " : ";
+
+          if (is_array($valuedata) or is_object($valuedata)) {
+            foreach ($valuedata as $dataSub => $valuedataSub) {
+              echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $dataSub . " => " . $valuedataSub;
+            }
+          } else {
+            continue;
+          }
+        }
+      } else {
+        continue;
+      }
+      echo '&nbsp;============<br>';
+    }
   }
 }
 
-
-// foreach ($encoded[0]['profile']['phones'] as $key => $value) {
-//   echo $value . "<br>";
-// }
+echo "<br><br>2. Find users who have articles.<br><br>";
+echo "<br><br>3. Find users who have 'annis' on their name.<br><br>";
+echo "<br><br>4. Find users who have articles on year 2020.<br><br>";
+echo "<br><br>5. Find users who are born on 1986.<br><br>";

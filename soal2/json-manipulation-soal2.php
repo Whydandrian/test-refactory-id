21df4489f88aa95b595602df8931479e65356124
Whydandrian <whydandrian@gmail.com>
2021-01-14 21:46:59 +0700

    add soal5

--- soal2/json-manipulation-soal2.php
+++ soal2/json-manipulation-soal2.php
@@ -1,9 +1,6 @@
 <?php
-setlocale(LC_ALL, 'id-ID', 'id_ID');
-
-
 $dataJson = file_get_contents("artikel.json");
-
+$encoded = json_decode($dataJson, true);
 if ($encoded = json_decode($dataJson, true)) {
 
   foreach ($encoded as $key => $value) {
@@ -13,9 +10,9 @@ if ($encoded = json_decode($dataJson, true)) {
       if (!(is_array($bValue))) {
         echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;" . $bKey . " : " . $bValue;
       } else {
-        if (is_array($bValue)) {
+        if (is_array($bValue) or is_object($valuedata)) {
           foreach ($bValue as $data => $valuedata) {
-            echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $data;
+            echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $data . " : ";
 
             if (is_array($valuedata) or is_object($valuedata)) {
               foreach ($valuedata as $dataSub => $valuedataSub) {

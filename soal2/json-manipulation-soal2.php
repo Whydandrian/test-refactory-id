985ad355693f8060575cc8a299fada751c9eab9c
Whydandrian <whydandrian@gmail.com>
2021-01-15 07:54:26 +0700

    modify soal2

--- soal2/json-manipulation-soal2.php
+++ soal2/json-manipulation-soal2.php
@@ -32,7 +32,7 @@ $encoded = json_decode($dataJson, true);
 // } else {
 //   echo 'error on syntax';
 // }
-echo "1. Find users who doesn't have any phone numbers.<br><br>";
+echo "<bold>1. Find users who doesn't have any phone numbers.</bold><br><br>";
 for ($a = 0; $a < count($encoded); $a++) {
 
   if (count($encoded[$a]['profile']['phones']) < 1) {
@@ -46,9 +46,40 @@ for ($a = 0; $a < count($encoded); $a++) {
         foreach ($value as $data => $valuedata) {
           echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $data . " : $valuedata";
 
-          if (is_array($valuedata) or is_object($valuedata)) {
-            foreach ($valuedata as $dataSub => $valuedataSub) {
-              echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $dataSub . " => " . $valuedataSub;
+          // if (is_array($valuedata) or is_object($valuedata)) {
+          //   foreach ($valuedata as $dataSub => $valuedataSub) {
+          //     echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $dataSub . " => " . $valuedataSub;
+          //   }
+          // } else {
+          //   continue;
+          // }
+        }
+      } else {
+        continue;
+      }
+      echo '&nbsp;============<br>';
+    }
+  }
+}
+
+echo "<br><br>2. Find users who have articles.<br><br>";
+
+for ($a = 0; $a < count($encoded); $a++) {
+
+  if (count($encoded[$a]['articles']) > 0) {
+    echo 'Data USER ID : ' . $a . '<br>';
+    echo '&nbsp;============<br>';
+
+    foreach ($encoded[$a] as $keyhasArticles => $valuehasArticles) {
+      echo "&nbsp;&nbsp;&nbsp;&nbsp;$keyhasArticles : $valuehasArticles <br>";
+
+      if (is_array($valuehasArticles) or is_object($valuehasArticles)) {
+        foreach ($valuehasArticles as $dataArticles => $valuedataArticles) {
+          echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $dataArticles . " : $valuedataArticles";
+
+          if (is_array($valuedataArticles) or is_object($valuedataArticles)) {
+            foreach ($valuedataArticles as $dataSubArticles => $valuedataSubArticles) {
+              echo "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $dataSubArticles . " => " . $valuedataSubArticles;
             }
           } else {
             continue;
@@ -62,7 +93,54 @@ for ($a = 0; $a < count($encoded); $a++) {
   }
 }
 
-echo "<br><br>2. Find users who have articles.<br><br>";
-echo "<br><br>3. Find users who have 'annis' on their name.<br><br>";
-echo "<br><br>4. Find users who have articles on year 2020.<br><br>";
-echo "<br><br>5. Find users who are born on 1986.<br><br>";
+
+echo "<br><br><bold>3. Find users who have 'annis' on their name.</bold><br><br>";
+
+for ($a = 0; $a < count($encoded); $a++) {
+  $datafullname = ($encoded[$a]['profile']['full_name'] . "<br>");
+
+  echo $datafullname;
+}
+
+
+echo "<br><br><bold>4. Find users who have articles on year 2020.</bold><br><br>";
+echo "Yang tidak pernah upload artikel yaitu : ";
+for ($a = 0; $a < count($encoded); $a++) {
+  $dataarticles = count($encoded[$a]['articles']);
+  if ($dataarticles == 0) {
+    echo $encoded[$a]['username'] . "<br>";
+  }
+}
+
+
+echo "<br><br><bold>5. Find users who are born on 1986.</bold><br><br>";
+echo "User yang lahir tahun 1986 yaitu : <br>";
+for ($a = 0; $a < count($encoded); $a++) {
+  $tgl = $encoded[$a]['profile']['birthday'];
+  $iduser = $encoded[$a]['id'];
+  $namauser = $encoded[$a]['username'];
+  $fullnameuser = $encoded[$a]['profile']['full_name'];
+  echo "&nbsp;&nbsp;&nbsp;&nbsp;" . $iduser . "<br>";
+  echo "&nbsp;&nbsp;&nbsp;&nbsp;" . $namauser . "<br>";
+  echo "&nbsp;&nbsp;&nbsp;&nbsp;" . $fullnameuser . "<br>";
+  echo "&nbsp;&nbsp;&nbsp;&nbsp;" . date('d F Y', strtotime($tgl)) . "<br> ===================== <br>";
+}
+
+echo "<br><br><bold>6. Find articles that contain 'tips' on the title.</bold><br><br>";
+$word = "tips";
+
+for ($a = 0; $a < count($encoded); $a++) {
+  $tipsData = $encoded[$a]['articles'];
+
+  foreach ($tipsData as $tipsKey => $valueTips) {
+
+    if (strpos($valueTips, $word) !== false) {
+      echo "&nbsp;&nbsp;&nbsp;&nbsp;" . $valueTips . "<br><br><br><br>";
+    } else {
+      echo "Word Not Found!";
+    }
+  }
+}
+
+
+echo "<br><br><bold>7. Find articles published before August 2019.</bold><br><br>";
